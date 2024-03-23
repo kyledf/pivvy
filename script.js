cd3df20611c80e7b0d16624420365d8cb3cd8837
kyleCS50 <kyledanielfrancis@gmail.com>
2024-03-23 15:59:39 +1300

    Added pony

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const click = document.querySelector(".click");
+const giftBox = document.querySelector(".gift-box");
+const shadow = document.querySelector(".shadow");
+const giftContainer = document.querySelector(".gift-container");
+const pony = document.querySelector(".pony");
 
 click.addEventListener("click", () => {
   if (click.className === "click") {
     click.classList.add("active");
+    giftBox.classList.add("active");
+    shadow.classList.add("active");
+    giftContainer.classList.add("active");
+    pony.classList.add("active");
+    pony.classList.remove("active2");
   } else {
     click.classList.remove("active");
+    giftBox.classList.remove("active");
+    shadow.classList.remove("active");
+    giftContainer.classList.remove("active");
+    pony.classList.remove("active");
+    pony.classList.add("active2");
   }
 });
