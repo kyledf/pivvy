b9b1c888ba86f14abf22ffca7010220c891c3a7d
kyleCS50 <kyledanielfrancis@gmail.com>
2024-03-28 12:43:43 +1300

    Added touch move event

--- catch/catch.js
+++ catch/catch.js
@@ -1,11 +1,18 @@
 const container = document.querySelector(".container");
 const player = document.querySelector(".player");
 
-container.onmousemove = function (e) {
+const movePlayer = (e) => {
   const x = e.clientX - container.offsetLeft;
   if (x > container.offsetWidth - player.offsetWidth) {
     player.style.left = container.offsetWidth - player.offsetWidth + "px";
     return;
   }
   player.style.left = x + "px";
+};
+
+container.onmousemove = function (e) {
+  movePlayer(e);
+};
+container.ontouchmove = function (e) {
+  movePlayer(e.touches[0]);
 };
