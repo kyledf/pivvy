32423746713bf64f52453af5d5e85d40a8cce282
kyleCS50 <kyledanielfrancis@gmail.com>
2024-03-28 14:00:04 +1300

    Add score when obj caught

--- catch/catch.js
+++ catch/catch.js
@@ -1,6 +1,14 @@
 const container = document.querySelector(".container");
 const player = document.querySelector(".player");
+const falling = document.getElementsByClassName("falling");
 
+//start game
+const startGame = () => {
+  moveFalling();
+  resetScore();
+};
+
+// make the player follow the mouse or touch position
 const movePlayer = (e) => {
   const x = e.clientX - container.offsetLeft;
   if (x > container.offsetWidth - player.offsetWidth) {
@@ -13,11 +21,67 @@ const movePlayer = (e) => {
 if ("ontouchstart" in document.documentElement) {
   container.ontouchmove = function (e) {
     movePlayer(e.touches[0]);
-    document.querySelector(".device").textContent = "Touch";
   };
 } else {
   container.onmousemove = function (e) {
     movePlayer(e);
-    document.querySelector(".device").textContent = "Mouse";
   };
 }
+
+// make the falling element move from top of container to bottom starting from random x position
+const moveFalling = (obj) => {
+  obj = falling[Math.floor(Math.random() * falling.length)];
+  obj.style.display = "block";
+  let containerX = container.offsetLeft;
+  let x = Math.floor(
+    Math.random() * (container.offsetWidth - obj.offsetWidth) + containerX
+  );
+  obj.style.left = x + "px";
+  obj.style.top = "0px";
+  //check if the falling element is catched by the player
+  checkCatch(obj);
+  //if the falling element reach the bottom of the container, it will be hidden
+  let id = setInterval(() => {
+    let y = obj.offsetTop;
+    if (y >= container.offsetHeight - obj.offsetHeight) {
+      obj.style.display = "none";
+      resetScore();
+      clearInterval(id);
+    } else {
+      obj.style.top = y + 1 + "px";
+    }
+  }, 10);
+};
+
+// check if the falling element is catched by the player
+const checkCatch = (obj) => {
+  let id = setInterval(() => {
+    let y = obj.offsetTop;
+    let x = obj.offsetLeft;
+    let playerX = player.offsetLeft;
+    let playerY = player.offsetTop;
+    //if the falling element is catched by the player, it will be hidden and the score will be increased when the object touches the player
+    if (
+      y >= playerY &&
+      y <= playerY + player.offsetHeight &&
+      x >= playerX &&
+      x <= playerX + player.offsetWidth
+    ) {
+      obj.style.display = "none";
+      clearInterval(id);
+      increaseScore();
+      moveFalling(obj);
+    }
+  }, 10);
+};
+
+//increase the score
+const increaseScore = () => {
+  let score = document.querySelector(".score-value");
+  score.textContent = parseInt(score.textContent) + 1;
+};
+//reset the score
+const resetScore = () => {
+  let score = document.querySelector(".score-value");
+  score.textContent = 0;
+};
