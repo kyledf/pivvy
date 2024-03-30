e7d91cd5ab1e70edab29b40e70f3984eb44b5324
kyleCS50 <kyledanielfrancis@gmail.com>
2024-03-30 17:12:32 +1300

    Added images

--- skincare/skincare.js
+++ skincare/skincare.js
@@ -1,31 +1,49 @@
 const cursor = document.querySelector(".cursor");
 const face = document.querySelector(".face");
+let activeProduct;
 
 activateProduct = (product) => {
   const isActive = document.querySelectorAll(".product.active");
   if (isActive[0]) {
     let active = isActive[0];
     isActive[0].classList.remove("active");
+    activeProduct = "";
     if (active === product) {
       return;
     }
   }
-  product.classList.toggle("active");
+  activeProduct = product.id;
+  console.log(activeProduct);
+  product.classList.add("active");
 };
 
 document.addEventListener("mousemove", (e) => {
+  cursor.style.top = e.pageY + "px";
+  cursor.style.left = e.pageX + "px";
   if (document.querySelector(".active") !== null) {
-    console.log("active");
-    cursor.style.display = "block";
-    cursor.style.top = e.pageY + "px";
-    cursor.style.left = e.pageX + "px";
+    cursor.style.opacity = "1";
+    if (activeProduct === "sink") {
+      cursor.style.backgroundImage = "url(../assets/skincare/water.webp)";
+    } else if (activeProduct === "towel") {
+      cursor.style.backgroundImage = "url(../assets/skincare/towel.png)";
+    } else if (activeProduct === "cleanser") {
+      cursor.style.backgroundImage = "url(../assets/skincare/cream.png)";
+    } else if (activeProduct === "toner") {
+      cursor.style.backgroundImage = "url(../assets/skincare/cotton-pad.webp)";
+    } else if (activeProduct === "moisturiser") {
+      cursor.style.backgroundImage = "url(../assets/skincare/cream.png)";
+    } else if (activeProduct === "face-mask") {
+      cursor.style.backgroundImage = "url(../assets/skincare/face-mask.png)";
+    } else if (activeProduct === "lip-balm") {
+      cursor.style.backgroundImage = "url(../assets/skincare/lip-balm.webp)";
+    }
   } else {
-    cursor.style.display = "none";
+    cursor.style.opacity = "0";
   }
 });
 
 face.addEventListener("mouseover", () => {
-  if (cursor.style.display === "block") {
+  if (cursor.style.opacity === "1") {
     face.style.backgroundColor = "blue";
   }
 });
