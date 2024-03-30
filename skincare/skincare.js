e5be6b3cb35e6f936f59c626c06fae7fde30dd7d
kyleCS50 <kyledanielfrancis@gmail.com>
2024-03-30 15:44:05 +1300

    Added animations

--- skincare/skincare.js
+++ skincare/skincare.js
@@ -1,21 +1,34 @@
-const products = document.querySelectorAll(".product");
+const cursor = document.querySelector(".cursor");
+const face = document.querySelector(".face");
 
-products.forEach((product) => {
-  product.addEventListener("click", () => {
-    const isActive = document.querySelectorAll(".product.active");
-    product.classList.toggle("active");
-    if (isActive[0]) {
-      isActive[0].classList.remove("active");
+activateProduct = (product) => {
+  const isActive = document.querySelectorAll(".product.active");
+  if (isActive[0]) {
+    let active = isActive[0];
+    isActive[0].classList.remove("active");
+    if (active === product) {
+      return;
     }
-  });
+  }
+  product.classList.toggle("active");
+};
+
+document.addEventListener("mousemove", (e) => {
+  if (document.querySelector(".active") !== null) {
+    console.log("active");
+    cursor.style.display = "block";
+    cursor.style.top = e.pageY + "px";
+    cursor.style.left = e.pageX + "px";
+  } else {
+    cursor.style.display = "none";
+  }
 });
 
-//make active product follow the mouse
-const activeProduct = document.querySelector(".product.active");
-document.addEventListener("mousemove", (e) => {
-  if (activeProduct) {
-    activeProduct.style.position = "absolute";
-    activeProduct.style.left = e.pageX + "px";
-    activeProduct.style.top = e.pageY + "px";
+face.addEventListener("mouseover", () => {
+  if (cursor.style.display === "block") {
+    face.style.backgroundColor = "blue";
   }
+});
+face.addEventListener("mouseout", () => {
+  face.style.backgroundColor = "white";
 });
