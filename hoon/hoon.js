5840dfc6be50fe74a759b007a56b7dd78d1734ec
kyleCS50 <kyledanielfrancis@gmail.com>
2024-04-19 15:27:59 +1200

    Fixed date

--- hoon/hoon.js
+++ hoon/hoon.js
@@ -1,11 +1,12 @@
 window.setInterval(function () {
   let now = Date.now();
-  let then = new Date("2024-04-10T14:35:00").getTime();
+  let then = new Date("2024-04-15T22:20:00").getTime();
   let distance = then - now;
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24)) * -1;
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24)) * -1 - 1;
   let hours =
     Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) * -1 - 1;
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) * -1;
+  let minutes =
+    Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) * -1 - 1;
   let seconds = Math.floor((distance % (1000 * 60)) / 1000) * -1 - 1;
   document.getElementById("time").innerHTML =
     days +
